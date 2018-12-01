b16ca25ff45b5cfa725c74873d8d4389a69ef3dc
Gayan Kaveenda <gayankaveenda@gmail.com>
2018-12-02 10:34:20 +1100

    added map services

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> {
+
+}
